3fbd4141b0e3d14a4e1761e50051342c47752ca9
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-03-07 21:22:01 +0900

    Remove 백준 JS folder

--- 백준 JS/bakjoon_template(fs).js
+++
@@ -1,8 +0,0 @@
-/* JS 기본 입력 - fs 모듈 */
-
-// fs를 이용해 파일 전체를 불러와서 처리
-// /dev/stdin 파일에 적힌 텍스트를 읽어온다고 가정
-
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-console.log(input);
